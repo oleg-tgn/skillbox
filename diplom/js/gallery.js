f83c955b4508c71dfd3f9a52c0ae323a72e57c20
Стельмах Олег Александрович <stelmakh@sfedu.ru>
2021-06-22 12:21:26 +0300

    Diplom. Fix gallery slider select for all sizes

--- diplom/js/gallery.js
+++ diplom/js/gallery.js
@@ -1,9 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var swiperGallery = new Swiper(".gallery__slider", {
+  const slides = {
+    painting: [
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-01.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-02.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-03.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-04.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-05.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-06.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-07.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-08.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-09.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-10.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-11.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-12.jpg')"></div></div>`,
+    ],
+    picture: [
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-13.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-14.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-15.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-16.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-17.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-18.jpg')"></div></div>`,      
+    ],
+    sculpture: [
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-19.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-20.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-21.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-22.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-23.jpg')"></div></div>`,
+      `<div class="swiper-slide"><div class="gallery__img" style="background-image: url('img/gallery-24.jpg')"></div></div>`,
+    ]
+  };
+
+  const swiperGallery = new Swiper("#gallery", {
     slidesPerView: 1,
     slidesPerColumn: 1,
     observer: true,
-    //spaceBetween: 10,
+    observeSlideChildren: true,
     pagination: {
       el: ".gallery-pagination",
       type: "fraction",
@@ -16,31 +49,22 @@ document.addEventListener('DOMContentLoaded', function() {
       768: {
         slidesPerView: 2,
         slidesPerColumn: 2,
-        //spaceBetween: 34,
       },
       1500: {
         slidesPerView: 3,
         slidesPerColumn: 2,
-        //spaceBetween: 50,
       },
     },
   });
 
+ 
+  swiperGallery.appendSlide(slides.painting);
 
   const selectElement = document.getElementById('galleryFilter');
-  const gallerySlide = document.querySelectorAll('.gallery__slider .swiper-slide');
 
   selectElement.addEventListener('change', (event) => {
-    for (let slide of gallerySlide) {
-     if (slide.dataset.gallerytype == event.target.value) {
-      slide.style.display = 'block';
-     } else {
-      slide.style.display = 'none';
-     }
-    }    
-    
+    swiperGallery.removeAllSlides();
+    swiperGallery.appendSlide(slides[event.target.value]);
     swiperGallery.slideTo(0);
-    swiperGallery.update();
-    
   });
 });
